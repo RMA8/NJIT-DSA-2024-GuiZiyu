3e9befda3c6831f199270e485f3a07c00c866749
RMA8 <2762729003@qq.com>
2024-03-13 19:05:10 +0800

    commit4

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -4,10 +4,6 @@ public class Grades {
    
    private Integer [] grades = null;
 
-   /**
-    * A constructor for building IntArrays.
-    * @param grades the plain Java integer array with numbers to add.
-    */
    public Grades(Integer [] grades) {
       this.grades = new Integer [grades.length];
       for (int counter = 0; counter < grades.length; counter++) {
@@ -16,14 +12,6 @@ public class Grades {
    }
 
    public void reverse() {
-      /* TODO:
-       1. Edit the test data files to see if the reverse() really works or not.
-       2. Execute the IntArrayTests to see that some of them fail.
-       3. Study the code below and try to find what is the issue.
-       4. Use the debugger to see the execution and variable values if necessary.
-       5. Fix the issue.
-       6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
-      */
       int i = 1;
       while (i <= grades.length/2) {
          int temp = grades[i-1];
@@ -34,14 +22,6 @@ public class Grades {
    }
 
    public void sort() {
-      /* TODO:
-       1. Edit the test data files to see if the sort() really works or not.
-       2. Execute the IntArrayTests to see that some of them fail.
-       3. Study the code below and try to find what is the issue.
-       4. Use the debugger to see the execution and variable values if necessary.
-       5. Fix the issue.
-       6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
-      */
       for (int i=0; i < grades.length-1; i++) {
          for (int j=0; j < grades.length-1; j++) {
             if(grades[j]>grades[j+1]){

e5a7fd6a219a1fe6dc0eef6144eb8b857fe8041a
RMA8 <2762729003@qq.com>
2024-03-21 11:49:32 +0800

    submit2

--- 02-1-stack/src/main/java/oy/tol/tra/StackImplementation.java
+++ 02-1-stack/src/main/java/oy/tol/tra/StackImplementation.java
@@ -1,16 +1,5 @@
 package oy.tol.tra;
 
-/**
- * An implementation of the StackInterface.
- * <p>
- * TODO: Students, implement this so that the tests pass.
- * 
- * Note that you need to implement construtor(s) for your concrete StackImplementation, which
- * allocates the internal Object array for the Stack:
- * - a default constructor, calling the StackImplementation(int size) with value of 10.
- * - StackImplementation(int size), which allocates an array of Object's with size.
- *  -- remember to maintain the capacity and/or currentIndex when the stack is manipulated.
- */
 public class StackImplementation<E> implements StackInterface<E> {
 
    private Object [] itemArray;
@@ -19,7 +8,6 @@ public class StackImplementation<E> implements StackInterface<E> {
    private static final int DEFAULT_STACK_SIZE = 10;
 
    /**
-    * Allocates a stack with a default capacity.
     * @throws StackAllocationException
     */
    public StackImplementation() throws StackAllocationException {
@@ -27,10 +15,7 @@ public class StackImplementation<E> implements StackInterface<E> {
       this(DEFAULT_STACK_SIZE);
    }
 
-   /** TODO: Implement so that
-    * - if the size is less than 2, throw StackAllocationException
-    * - if the allocation of the array throws with Java exception,
-    *   throw StackAllocationException.
+   /**
     * @param capacity The capacity of the stack.
     * @throws StackAllocationException If cannot allocate room for the internal array.
     */
@@ -49,13 +34,11 @@ public class StackImplementation<E> implements StackInterface<E> {
 
    @Override
    public int capacity() {
-      // TODO: Implement this
       return capacity;
    }
 
    @Override
    public void push(E element) throws StackAllocationException, NullPointerException {
-      // TODO: Implement this
       if (currentIndex == capacity - 1) {
          int newCapacity = capacity * 2;
          Object[] newArray = new Object[newCapacity];
@@ -93,13 +76,11 @@ public class StackImplementation<E> implements StackInterface<E> {
 
    @Override
    public int size() {
-      // TODO: Implement this
       return currentIndex + 1;
    }
 
    @Override
    public void clear() {
-      // TODO: Implement this
       for (int i = 0; i <= currentIndex; i++) {
          itemArray[i] = null;
       }
@@ -108,7 +89,6 @@ public class StackImplementation<E> implements StackInterface<E> {
 
    @Override
    public boolean isEmpty() {
-      // TODO: Implement this
       return currentIndex == -1;
       
    }

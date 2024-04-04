5a7c6e2735da2e72376c634f797b1909933bcc3d
RMA8 <2762729003@qq.com>
2024-04-04 23:10:53 +0800

    commit

--- 06-dailytasks/src/main/java/oy/tol/tra/DailyTasks.java
+++ 06-dailytasks/src/main/java/oy/tol/tra/DailyTasks.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/**
- * A class showing your daily schedule using a timer.
- */
 public class DailyTasks {
 
    private QueueInterface<String> dailyTaskQueue = null;
@@ -27,26 +24,16 @@ public class DailyTasks {
 
    private void run() {
       try {
-         // TODO:
-         // 1. create a queue (to the member variable!) for daily tasks, which are strings.
          dailyTaskQueue=new QueueImplementation<>();
-         // 2. read the tasks for today by calling readTasks() -- implementing missing parts of it!
          readTasks();
-         // 3. create Java Timer object (to member variable) to schedule your daily tasks. (Already given to you.)
          timer = new Timer();
-         // 4. schedule the timer at fixed rate with a new TimerTask,
-         //  using the delay constant values in the class member variable. (Already given to you.)
          timer.scheduleAtFixedRate(new TimerTask() {
-            // 4.1 in the timer task run:
             @Override
             public void run() {
-               // 4.1.1 check if there are tasks in the queue:
                if (!dailyTaskQueue.isEmpty()) {
-                  // 4.1.2 if yes, print the task from the queue, dequeueing it.
                   String task = dailyTaskQueue.dequeue();
                   System.out.println("Task: " + task);
                }else {
-                  // 4.1.3 if not, cancel the timer.
                   timer.cancel();
                }
             }
@@ -61,10 +48,8 @@ public class DailyTasks {
       tasks = new String(getClass().getClassLoader().getResourceAsStream("DailyTasks.txt").readAllBytes());
       String[] allTasks = tasks.split("\\r?\\n");
       for (String task : allTasks) {
-         // TODO: Enqueue the task to your Queue implementation:
          dailyTaskQueue.enqueue(task);
       }
-      // TODO: print out to the console the number of tasks in the queue:
       System.out.println("Number of tasks in the queue: " + dailyTaskQueue.size());
    }
 }
--- 06-dailytasks/src/main/java/oy/tol/tra/QueueImplementation.java
+++ 06-dailytasks/src/main/java/oy/tol/tra/QueueImplementation.java
@@ -4,24 +4,22 @@ public class QueueImplementation<E> implements QueueInterface<E> {
 
 	private Object[] itemArray;
 	private int capacity;
-	private int front;
-	private int rear;
-
-	private int tempElementSize = 0;
-
+	private int head;
+	private int tail;
+	private int size = 0;
 	private static final int DEFAULT_QUEUE_SIZE = 10;
 
-	public QueueImplementation() throws QueueAllocationException {
-		this(DEFAULT_QUEUE_SIZE);
-	}
-
-	public QueueImplementation(int capacity) throws QueueAllocationException {
+    public QueueImplementation(int capacity) throws QueueAllocationException {
 		if (capacity < 1) {
 			throw new QueueAllocationException("Invalid capacity");
 		}
 		itemArray = new Object[capacity];
 		this.capacity = capacity;
-		front = rear = -1;
+		head = tail = -1;
+	}
+
+	public QueueImplementation() throws QueueAllocationException {
+		this(DEFAULT_QUEUE_SIZE);
 	}
 
 	@Override
@@ -34,17 +32,16 @@ public class QueueImplementation<E> implements QueueInterface<E> {
 		if (element == null) {
 			throw new NullPointerException("Element cannot be NULL");
 		}
-		if (tempElementSize == capacity) {
-			//todo capacity*2
-			doubleCapacity();
+		if (size == capacity) {
+		 relocate();
 		}
 		if (isEmpty()) {
-			front = rear = 0;
+			head = tail = 0;
 		} else {
-			rear = (rear + 1) % capacity;
+			tail = (tail + 1) % capacity;
 		}
-		itemArray[rear] = element;
-		tempElementSize++;
+		itemArray[tail] = element;
+		size++;
 	}
 
 	@Override
@@ -53,23 +50,45 @@ public class QueueImplementation<E> implements QueueInterface<E> {
 		if (isEmpty()) {
 			throw new QueueIsEmptyException("Queue is empty");
 		}
-		E element = (E) itemArray[front];
-		tempElementSize--;
-		if (front == rear) {
-			front = rear = -1;
+		E element = (E) itemArray[head];
+		size--;
+		if (head == tail) {
+			head = tail = -1;
 		} else {
-			front = (front + 1) % capacity;
+			head = (head + 1) % capacity;
 		}
 		return element;
+	}
+
+    private void relocate() throws QueueAllocationException {
+		int newCapacity = capacity * 2;
+		Object[] newArray = new Object[newCapacity];
+		if (head <= tail) {
+			for (int i = 0; i < capacity; i++) {
+				newArray[i] = itemArray[i];
+			}
+		} else {
+			int j = 0;
+			for (int i = head; i < capacity; i++) {
+				newArray[j++] = itemArray[i];
+			}
+			for (int i = 0; i <= tail; i++) {
+				newArray[j++] = itemArray[i];
+			}
+		}
+		itemArray = newArray;
+		head = 0;
+		tail = capacity - 1;
+		capacity = newCapacity;
 	}
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public E element() throws QueueIsEmptyException {
 		if (isEmpty()) {
-			throw new QueueIsEmptyException("Queue is empty");
+			throw new QueueIsEmptyException("Queue cannot be empty");
 		}
-		return (E) itemArray[front];
+		return (E) itemArray[head];
 	}
 
 	@Override
@@ -77,38 +96,38 @@ public class QueueImplementation<E> implements QueueInterface<E> {
 		if (isEmpty()) {
 			return 0;
 		}
-		return tempElementSize;
+		return size;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return tempElementSize == 0;
+		return size == 0;
 	}
 
 	@Override
 	public void clear() {
-		front = rear = -1;
-		tempElementSize = 0;
+		head = tail = -1;
+		size = 0;
 	}
 
 	public String toString() {
 		StringBuilder builder = new StringBuilder("[");
 		if (!isEmpty()) {
-			if (front <= rear) {
-				for (int i = front; i <= rear; i++) {
+			if (head <= tail) {
+				for (int i = head; i <= tail; i++) {
 					builder.append(itemArray[i]);
-					if (i < rear) {
+					if (i < tail) {
 						builder.append(", ");
 					}
 				}
 			} else {
-				for (int i = front; i < capacity; i++) {
+				for (int i = head; i < capacity; i++) {
 					builder.append(itemArray[i]);
 					builder.append(", ");
 				}
-				for (int i = 0; i <= rear; i++) {
+				for (int i = 0; i <= tail; i++) {
 					builder.append(itemArray[i]);
-					if (i < rear) {
+					if (i < tail) {
 						builder.append(", ");
 					}
 				}
@@ -118,26 +137,4 @@ public class QueueImplementation<E> implements QueueInterface<E> {
 		return builder.toString();
 	}
 
-	private void doubleCapacity() throws QueueAllocationException {
-		int newCapacity = capacity * 2;
-		Object[] newArray = new Object[newCapacity];
-		if (front <= rear) {
-			for (int i = 0; i < capacity; i++) {
-				newArray[i] = itemArray[i];
-			}
-		} else {
-			// copy
-			int j = 0;
-			for (int i = front; i < capacity; i++) {
-				newArray[j++] = itemArray[i];
-			}
-			for (int i = 0; i <= rear; i++) {
-				newArray[j++] = itemArray[i];
-			}
-		}
-		itemArray = newArray;
-		front = 0;
-		rear = capacity - 1;
-		capacity = newCapacity;
-	}
 }
